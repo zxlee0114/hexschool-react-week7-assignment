ecffc8fa6e105014ee9bdc5e2d3f689d4860e920
zxlee0114 <napoleon.lee0114@gmail.com>
2025-03-08 15:26:53 +0800

    feat: 於 toastSlice 建立 action - pushMessage

--- src/redux/slices/toastSlice.js
+++ src/redux/slices/toastSlice.js
@@ -6,6 +6,19 @@ const initialState = {
 const toastSlice = createSlice({
   name: "toast",
   initialState,
+  reducers: {
+    pushMassage(state, action) {
+      const { text, status } = action.payload;
+      const id = Date.now();
+
+      state.messages.push({
+        id,
+        text,
+        status,
+      });
+    },
+  },
 });
 
+export const { pushMassage } = toastSlice.actions;
 export default toastSlice.reducer;
